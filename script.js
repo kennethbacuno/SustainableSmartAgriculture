de3d420ec0b534fe81e339a7eb7f45f323339789
Kenneth <bacunoabnerkenneth@gmail.com>
2024-11-03 15:45:24 +0800

    sige na nga

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ function showSection(sectionId) {
     document.getElementById(sectionId).style.display = 'block';
     closeNav();
 }
+
+function toggleWaterPump() {
+    const toggle = document.getElementById("toggleWaterPump");
+    var controlCard = document.getElementById("water-control");
+    if (toggle.checked) {
+        controlCard.style.backgroundColor = "#12807c";
+        controlCard.querySelector("p strong").textContent = "ON";
+    } else {
+        controlCard.style.backgroundColor = "#C2D6C5";
+        controlCard.querySelector("p strong").textContent = "OFF";
+    }
+}
+
+function toggleFertilizerPump() {
+    const toggle = document.getElementById("toggleFertilizerPump");
+    var controlCard = document.getElementById("fertilizer-control");
+    if (toggle.checked) {
+        controlCard.style.backgroundColor = "#12807c";
+        controlCard.querySelector("p strong").textContent = "ON";
+    } else {
+        controlCard.style.backgroundColor = "#C2D6C5";
+        controlCard.querySelector("p strong").textContent = "OFF";
+    }
+}
+
+function toggleRoof() {
+    const toggle = document.getElementById("toggleRoof");
+    var controlCard = document.getElementById("roof-control");
+    if (toggle.checked) {
+        controlCard.style.backgroundColor = "#12807c";
+        controlCard.querySelector("p strong").textContent = "OPEN";
+    } else {
+        controlCard.style.backgroundColor = "#C2D6C5";
+        controlCard.querySelector("p strong").textContent = "CLOSED";
+    }
+}
